286e05ce3a661681f093654bf797dba3eebe5b3c
Brian Helenbrook <bhelenbr@clarkson.edu>
2026-01-06 18:49:10 -0500

    Getting close to final

--- scatter_all.bash
+++ scatter_all.bash
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-advisee_counts_scatter.py "$1"/CU_ACAD_DEPT_ADVISOR_CT_ALLDEP_* $2
+employee_id_scatter.py "$1"/Faculty\ List* "$2"
+advisee_counts_scatter.py "$1"/CU_ACAD_DEPT_ADVISOR_CT_ALLDEP_* "$2"
 for file in "$1"/CU_ADVIS_EVAL_DATA_ALL*; do
 	advising_eval_scatter.py "$file" "$2"; done
 srs_scatter.py "$1"/CU_GM_ALL_PROPS_ALL_* "$2"
@@ -8,6 +9,8 @@ current_grads_scatter2.py "$1"/CU_ACAD_STDNT_ADVISOR_LISTING* "$2"
 teaching_eval_scatter.py "$1"/CU_CRSE_EVAL_DATA_ALL_* "$2"
 for file in "$1"/CU_BH_PROJ_RES_CLASS_FULL*; do
 	UR_scatter.py "$file" "$2"; done
+#UR_honors_scatter.py "$1"/FAR\ 2025* "$2"
+#UR_mcnair_scatter.py "$1"/McNair* "$2"
 prospective_scatter.py "$1"/Personal\ Visit\ Information.xlsx "$2"
 expenditures_scatter.py "$1"/CU_INTERFOLIO_ADDITIONAL_DATA_* "$2"
 service_scatter.py -y 2025 "$1"/Service\ Master* "$2"
