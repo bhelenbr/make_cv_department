7e072f802399a173c763bf2776f9ae5354163150
Brian Helenbrook <bhelenbr@clarkson.edu>
2026-01-08 11:45:19 -0500

    Small update to reset_files

--- reset_files.sh
+++ reset_files.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
 # This script resets folder
-cd "$1"
+cd "$2"
 for file in *; do
 	rm ${file}/*/*.xlsx;
-	reset.bash ~/Codes/make_cv/src/make_cv/files $file xlsx;
+	reset.bash "$1/src/make_cv/files" $file xlsx;
 	rm -rf ${file}/make_cv/FAR_docx
-	cp -r ~/Codes/make_cv/src/make_cv/files/make_cv/FAR_docx ${file}/make_cv;
+	cp -r "$1/src/make_cv/files/make_cv/FAR_docx" ${file}/make_cv;
 	replace_line.sh ${file}/make_cv/FAR/make_cv.cfg ${file}/make_cv/FAR_docx/make_cv.cfg "googleid =";
 	replace_line.sh ${file}/make_cv/FAR/make_cv.cfg ${file}/make_cv/FAR_docx/make_cv.cfg "orcid =";
 done
