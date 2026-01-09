f5ceb3841bc2eb2338d8f468c2c61316b74ba77d
Brian Helenbrook <bhelenbr@clarkson.edu>
2026-01-09 16:43:19 -0500

    Changed reset_files to use theses.bib

--- reset_files.sh
+++ reset_files.sh
@@ -1,13 +1,17 @@
 #!/bin/zsh
 
-# This script resets folder
+# This script resets folders
+# Argument 1 is the location of make_cv
+# Argument 2 is the department folder
+
 cd "$2"
 for file in *; do
 	rm ${file}/*/*.xlsx;
 	reset.bash "$1/src/make_cv/files" $file xlsx;
 	rm -rf ${file}/make_cv/FAR_docx
 	cp -r "$1/src/make_cv/files/make_cv/FAR_docx" ${file}/make_cv;
-	replace_line.sh ${file}/make_cv/FAR/make_cv.cfg ${file}/make_cv/FAR_docx/make_cv.cfg "googleid =";
-	replace_line.sh ${file}/make_cv/FAR/make_cv.cfg ${file}/make_cv/FAR_docx/make_cv.cfg "orcid =";
+	replace_line.sh "${file}/make_cv/FAR/make_cv.cfg" "${file}/make_cv/FAR_docx/make_cv.cfg" "googleid =";
+	replace_line.sh "${file}/make_cv/FAR/make_cv.cfg" "${file}/make_cv/FAR_docx/make_cv.cfg" "orcid =";
+	replace_line.sh "${file}/make_cv/FAR_docx/make_cv.cfg" "${file}/make_cv/Collaborators/make_cv.cfg" "gradthesesfile =";
 done
 	 
